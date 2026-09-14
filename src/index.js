17749dc0fdb78298e7f9e2cecf22190b9ffba882
CHIRAG9066 <chiragbansal9066@gmail.com>
2026-09-14 23:17:56 +0530

    connection of database

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+
+dotenv.config({path: "./.env"});;
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+
+(async () => {
+  try{
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (err) => {
+      console.error("ERROR:",err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+
+
+  }catch(err){
+    console.error("ERROR:",error);
+    throw err;
+  }
+})();
+*/
